15cc1faf8f154abd14816979c8c55c533637b5de
Daniel Bosen <daniel.bosen@burda.com>
2017-04-28 17:29:12 +0200

    source exports

--- scripts/travis/before-install.sh
+++ scripts/travis/before-install.sh
@@ -3,5 +3,3 @@
 # Remove xdebug to make php execute faster.
 phpenv config-rm xdebug.ini
 
-# Add composer bin path.
-export PATH="$HOME/.composer/vendor/bin:$PATH"
---
+++ scripts/travis/setup-environment.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+# Add composer bin path.
+export PATH="$HOME/.composer/vendor/bin:$PATH"
+
