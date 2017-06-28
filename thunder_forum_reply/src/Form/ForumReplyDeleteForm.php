40579f734f2e1650cbcf21af7e43b712276de297
Thomas Keitel <tom@undpaul.de>
2017-06-28 10:29:23 +0200

    DKRFRNW-72: Improve forum reply delete form

--- thunder_forum_reply/src/Form/ForumReplyDeleteForm.php
+++ thunder_forum_reply/src/Form/ForumReplyDeleteForm.php
@@ -9,4 +9,44 @@ use Drupal\Core\Entity\ContentEntityDeleteForm;
  */
 class ForumReplyDeleteForm extends ContentEntityDeleteForm {
 
+  /**
+   * {@inheritdoc}
+   */
+  public function getCancelUrl() {
+    return $this->entity->toUrl();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getRedirectUrl() {
+    /** @var \Drupal\thunder_forum_reply\ForumReplyInterface $entity */
+    $entity = $this->entity;
+
+    return $entity->getRepliedNode()->toUrl();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getDescription() {
+    return $this->t('Any related responses to this forum reply will be lost. This action cannot be undone.');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getDeletionMessage() {
+    return $this->t('The forum reply and all its related responses have been deleted.');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function logDeletionMessage() {
+    $this->logger('thunder_forum_reply')->notice('Deleted forum reply @frid and its related responses.', [
+      '@frid' => $this->entity->id(),
+    ]);
+  }
+
 }
